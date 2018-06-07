9f438f5549578f746574dd9b766350449fc670a8
Rafael Furry <Rafa@Rafaels-MacBook-Pro.local>
2018-06-07 16:01:57 -0700

    Create MapService

---
+++ client/src/services/MapService.js
@@ -0,0 +1,161 @@
+export default class MapService {
+  statesColor() {
+    this.dem = '#6a8fc3';
+    this.rep = '#ee7868';
+    return {
+      AL: {
+        fill: this.rep,
+      },
+      AK: {
+        fill: this.rep,
+      },
+      AZ: {
+        fill: this.rep,
+      },
+      AR: {
+        fill: this.rep,
+      },
+      CA: {
+        fill: this.dem,
+      },
+      CO: {
+        fill: this.dem,
+      },
+      CT: {
+        fill: this.dem,
+      },
+      DC: {
+        fill: this.dem,
+      },
+      DE: {
+        fill: this.dem,
+      },
+      FL: {
+        fill: this.rep,
+      },
+      GA: {
+        fill: this.rep,
+      },
+      HI: {
+        fill: this.dem,
+      },
+      ID: {
+        fill: this.rep,
+      },
+      IL: {
+        fill: this.dem,
+      },
+      IN: {
+        fill: this.rep,
+      },
+      IA: {
+        fill: this.rep,
+      },
+      KS: {
+        fill: this.rep,
+      },
+      KY: {
+        fill: this.rep,
+      },
+      LA: {
+        fill: this.rep,
+      },
+      ME: {
+        fill: this.dem,
+      },
+      MD: {
+        fill: this.dem,
+      },
+      MA: {
+        fill: this.dem,
+      },
+      MI: {
+        fill: this.rep,
+      },
+      MN: {
+        fill: this.dem,
+      },
+      MS: {
+        fill: this.rep,
+      },
+      MO: {
+        fill: this.rep,
+      },
+      MT: {
+        fill: this.rep,
+      },
+      NE: {
+        fill: this.rep,
+      },
+      NV: {
+        fill: this.dem,
+      },
+      NH: {
+        fill: this.dem,
+      },
+      NJ: {
+        fill: this.dem,
+      },
+      NM: {
+        fill: this.dem,
+      },
+      NY: {
+        fill: this.dem,
+      },
+      NC: {
+        fill: this.rep,
+      },
+      ND: {
+        fill: this.rep,
+      },
+      OH: {
+        fill: this.rep,
+      },
+      OK: {
+        fill: this.rep,
+      },
+      OR: {
+        fill: this.dem,
+      },
+      PA: {
+        fill: this.rep,
+      },
+      RI: {
+        fill: this.dem,
+      },
+      SC: {
+        fill: this.rep,
+      },
+      SD: {
+        fill: this.rep,
+      },
+      TN: {
+        fill: this.rep,
+      },
+      TX: {
+        fill: this.rep,
+      },
+      UT: {
+        fill: this.rep,
+      },
+      VT: {
+        fill: this.dem,
+      },
+      VA: {
+        fill: this.dem,
+      },
+      WA: {
+        fill: this.dem,
+      },
+      WV: {
+        fill: this.rep,
+      },
+      WI: {
+        fill: this.rep,
+      },
+      WY: {
+        fill: this.rep,
+      },
+    };
+  }
+}
