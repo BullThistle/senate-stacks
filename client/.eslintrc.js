76f471db51e2fd28119600063bbb1de6d0f8a39a
Rafael Furry <Rafa@Rafaels-MacBook-Pro.local>
2018-06-07 11:56:12 -0700

    Update eslint rules

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
-    "extends": "airbnb"
+  extends: 'airbnb',
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off'
+  },
+  env: {
+    browser: true,
+    es6: true
+  }
 };
