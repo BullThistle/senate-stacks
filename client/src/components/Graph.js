1315958fb1a4516a7bff775ee85cb122dd57694c
Rafael Furry <Rafa@Rafaels-MacBook-Pro.local>
2018-06-12 07:08:41 -0700

    Create Graph component

---
+++ client/src/components/Graph.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Grid } from 'semantic-ui-react';
+import { PieChart, Pie, Tooltip } from 'recharts';
+
+export default class Graph extends Component {
+  constructor(props) {
+    super(props);
+    this.data = [
+      {
+        name: this.props.obj['0']['@attributes'].org_name,
+        value: this.props.obj['0']['@attributes'].total / 1000,
+      },
+      {
+        name: this.props.obj['1']['@attributes'].org_name,
+        value: this.props.obj['1']['@attributes'].total / 1000,
+      },
+      {
+        name: this.props.obj['2']['@attributes'].org_name,
+        value: this.props.obj['2']['@attributes'].total / 1000,
+      },
+      {
+        name: this.props.obj['3']['@attributes'].org_name,
+        value: this.props.obj['3']['@attributes'].total / 1000,
+      },
+      {
+        name: this.props.obj['4']['@attributes'].org_name,
+        value: this.props.obj['4']['@attributes'].total / 1000,
+      },
+      {
+        name: this.props.obj['5']['@attributes'].org_name,
+        value: this.props.obj['5']['@attributes'].total / 1000,
+      },
+      {
+        name: this.props.obj['6']['@attributes'].org_name,
+        value: this.props.obj['6']['@attributes'].total / 1000,
+      },
+      {
+        name: this.props.obj['7']['@attributes'].org_name,
+        value: this.props.obj['7']['@attributes'].total / 1000,
+      },
+      {
+        name: this.props.obj['8']['@attributes'].org_name,
+        value: this.props.obj['8']['@attributes'].total / 1000,
+      },
+      {
+        name: this.props.obj['9']['@attributes'].org_name,
+        value: this.props.obj['9']['@attributes'].total / 1000,
+      },
+    ];
+  }
+
+  render() {
+    return (
+      <Grid className="centered">
+        <p>Top contributors (in thousands)</p>
+        <PieChart width={1000} height={400}>
+          <Pie
+            data={this.data}
+            cx={500}
+            cy={200}
+            innerRadius={60}
+            outerRadius={120}
+            fill="#6a8fc3"
+            label
+          />
+          <Tooltip />
+        </PieChart>
+      </Grid>
+    );
+  }
+}
+
+Graph.propTypes = {
+  obj: PropTypes.shape({
+    0: PropTypes.string.isRequired,
+    1: PropTypes.string.isRequired,
+    2: PropTypes.string.isRequired,
+    3: PropTypes.string.isRequired,
+    4: PropTypes.string.isRequired,
+    5: PropTypes.string.isRequired,
+    6: PropTypes.string.isRequired,
+    7: PropTypes.string.isRequired,
+    8: PropTypes.string.isRequired,
+    9: PropTypes.string.isRequired,
+  }).isRequired,
+};
