2584de1b7ece1c5a2fdb3ff93929c161f5b18c3a
Rafael Furry <Rafa@Rafaels-MacBook-Pro.local>
2018-07-01 16:49:05 -0700

    Add /legislator/:cid route to client

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HeaderMenu from './components/HeaderMenu';
 import Home from './components/Home';
 import State from './components/State';
+import Legislator from './components/Legislator';
 
 const App = () => (
   <div>
@@ -11,7 +12,8 @@ const App = () => (
         <HeaderMenu />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/:states" component={State} />
+          <Route exact path="/:states" component={State} />
+          <Route path="/legislator/:cid" component={Legislator} />
         </Switch>
       </div>
     </Router>
